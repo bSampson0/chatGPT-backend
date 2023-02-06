bf2b8e78f163eb4fe3ee9b2b9a2c8ec1ef5f8aae
bnicerrr <bnicerrr@gmail.com>
2023-02-06 09:42:50 -0800

    initial push

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { ChatGPTAPI } from "chatgpt";
+
+const app = express();
+const port = 6969;
+const api = new ChatGPTAPI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+app.use(express.json());
+
+const getData = async (message) => {
+  const res = await api.sendMessage(message);
+  return res;
+};
+
+app.post("/", async (req, res) => {
+  req.body;
+  const data = await getData(req.body.message);
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
